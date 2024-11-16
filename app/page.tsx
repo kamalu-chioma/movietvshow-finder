9649ece637c0d0f0bfe500ec057571aaf635819a
Chioma Kamalu <kamaluchioma062@gmail.com>
2024-11-16 16:46:40 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -82,7 +82,7 @@ const Home = () => {
     
       const normalizedScore = wishlist.length > 0 ? totalSimilarity / wishlist.length : 0;
     
-      // Adding a small random variation to avoid identical scores
+      //return cosine similarity
       return (normalizedScore * 80) + Math.random() * 5;
     };
     
